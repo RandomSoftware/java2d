edefc03a418acac78eb43cee47e6ba609323877e
msventukh <msventukh@gmail.com>
2026-08-17 22:28:02 +0200

    fix: fix corner case for MiddlePointLineGenerator

--- src/main/kotlin/ru/randomsoftware/java2d/generators/MiddlePointLineGenerator.kt
+++ src/main/kotlin/ru/randomsoftware/java2d/generators/MiddlePointLineGenerator.kt
@@ -2,6 +2,6 @@ package ru.randomsoftware.java2d.generators
 
 class MiddlePointLineGenerator : LineGenerator<Boolean> {
   override fun generate(size: Int): List<Boolean> {
-    return (1..size).map { it == size / 2 }
+    return (1..size).map { it == (size + 1) / 2 }
   }
 }
